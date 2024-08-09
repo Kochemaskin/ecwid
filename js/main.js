3abe5cfdc68982380bf34dfcbd6b9e0a0669f1c8
Kochemaskin <57582915+Kochemaskin@users.noreply.github.com>
2024-08-09 03:31:46 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-Ecwid.OnAPILoaded.add(function() {
-  console.log("Ecwid storefront JS API has loaded");
-});
-
-
-
 document.write("JS says hello");
 
 function pickItem1(image) {
@@ -51,58 +45,3 @@ function checkAllPressed() {
     document.getElementById('text1').innerText = 'Соберите автомобиль';
   }
 }
-/*function before(){
-    document.getElementById('myImage')
-    .src="img/photo1.jpg";
-    document.getElementById('message')
-    .innerHTML="Hii! GeeksforGeeks people";
-}
-     
-function afterr(){
-    document.getElementById('myImage')
-    .src="img/photo2.jpg";
-    document.getElementById('message')
-    .innerHTML="Bye! GeeksforGeeks people";
-}
-function pickItem(imgId) {
-  const image = document.getElementById(imgId);
-  const baseName = image.src.match(/image_part_\d{3}/)[0];
-
-  if (image.src.includes(`${baseName}_pressed.jpg`)) {
-    image.src = `resources/${baseName}.jpg`;
-  } else {
-    image.src = `resources/${baseName}_pressed.jpg`;
-  }
-}
-/*
-
-function pickItem() {
-  let image = document.getElementById('img1');
-  if (image.src.includes('resources/image_part_001_pressed.jpg')) {
-      image.src = 'resources/image_part_001.jpg';
-  } else {
-      image.src = 'resources/image_part_001_pressed.jpg';
-  }
-}
-*/
-
-
-/*
-var product = {
-    id: 10,
-    quantity: 3,
-    options: {
-      "Size": "L"
-    },
-    recurringChargeSettings: { 
-      recurringInterval: "month",
-      recurringIntervalCount: 1,
-      },
-    callback: function(success, product, cart) {
-      // ...  
-    }
-  }
-  
-  Ecwid.Cart.addProduct(product);
-
-  */
